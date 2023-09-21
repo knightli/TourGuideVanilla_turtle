8d950266d1f60f7b875012f2643b86565bfccca1
knightli <knightli@foxmail.com>
2023-09-21 10:06:36 +0800

    init

---
+++ NilGuide.lua
@@ -0,0 +1,4 @@
+local L = TourGuide.Locale
+
+TourGuide:RegisterGuide("No Guide", nil, "Alliance", function() return L["K 未装载任务导航... |N|点击，选择指南|"] end)
+TourGuide:RegisterGuide("No Guide", nil, "Horde", function() return L["K 未装载任务导航... |N|点击，选择指南|"] end)
